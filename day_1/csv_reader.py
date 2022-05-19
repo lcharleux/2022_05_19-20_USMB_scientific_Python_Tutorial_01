39bd694093f993e93c8bed0b3516ed2408b61cc4
lcharleux <ludovic.charleux@univ-smb.fr>
2022-05-19 13:46:23 +0200

    Curve_fitting example

--- day_1/csv_reader.py
+++ day_1/csv_reader.py
@@ -12,11 +12,27 @@ for file in files:
     if file.endswith(".csv"):
         d = pd.read_csv(datadir + file,
         header =0, index_col=0)
+        file_num = int(file.split(".")[0].split("_")[1])
+        d["file"] = file_num
         data.append(d)
 
+data = pd.concat(data, ignore_index = True)
+
+from scipy import optimize
+# CURVE FITTING
+def func(x, a, b):
+    return b * np.cos(a * x) * x**2 
+opt, cov = optimize.curve_fit(func, 
+    data.x, 
+    data.y, 
+    p0 = [3.2,4])
+aopt, bopt = opt # OPTIMAL VALUES OF a AND b
+x = data[data.file == 0].x
+
 plt.figure()
-for d in data:
-    plt.scatter(d.x, d.y, s = 2)
+plt.scatter(data.x, data.y, s = 1)
+plt.plot(x, func(x, aopt, bopt), "r-")
 plt.show()    
+
 
 
